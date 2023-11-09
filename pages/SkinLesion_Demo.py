73f7e990f55a008c12efe5fa24f4d195915a48a4
KylePhillips4637 <108509804+KylePhillips4637@users.noreply.github.com>
2023-11-09 19:36:52 +0000

    Co-authored-by: jgiordano7280 <jgiordano7280@users.noreply.github.com> Co-authored-by: Chorizoman01 <Chorizoman01@users.noreply.github.com> Co-authored-by: Aron Chen <aronchen7216@gmail.com> Co-authored-by: Luan Nguyen <luandknguyen@users.noreply.github.com>

---
+++ pages/SkinLesion_Demo.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+# Load the model (make sure to provide the path to the directory containing saved_model.pb and variables folder)
+model = tf.keras.models.load_model('../models/trained')
+
+# Function to preprocess the uploaded image
+def preprocess_image(uploaded_image):
+    image = Image.open(uploaded_image).resize((512, 512))
+    image_array = np.array(image) / 255.0  # Scale pixel values if required
+    image_array = image_array[np.newaxis, ..., :3]  # Add batch dimension and remove alpha channel if present
+    return image_array
+
+# Function to postprocess the prediction
+def postprocess_prediction(prediction):
+    mask = prediction > 0.5  # Threshold the predictions to get binary mask
+    mask = mask.squeeze() * 255  # Remove batch dimension and convert to uint8
+    return Image.fromarray(mask.astype(np.uint8))
+
+# Streamlit interface
+st.title('Skin Lesion Identification App')
+uploaded_file = st.file_uploader("Choose a skin image...", type="jpg")
+
+if uploaded_file is not None:
+    image = preprocess_image(uploaded_file)
+    st.image(image.squeeze(), caption='Uploaded Skin Image', use_column_width=True)
+    
+    if st.button('Identify Lesion'):
+        prediction = model.predict(image)
+        mask_image = postprocess_prediction(prediction)
+        st.image(mask_image, caption='Predicted Lesion Mask', use_column_width=True)
